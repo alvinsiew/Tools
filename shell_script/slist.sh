55ee412de87caf18535c0437247d19f74efb32bf
Alvin Siew <alvin_siew@tech.gov.sg>
2018-10-09 18:51:10 +0800

    Upgraded with colours

--- shell_script/slist.sh
+++ shell_script/slist.sh
@@ -7,11 +7,18 @@ rm -f $list_path
 list=`cat ~/.ssh/config | grep "Host " | awk '{print $2}'`
 num=1
 
+colour=34
 for line in $list
 do
 	sn=$num
-	echo -ne "$num $line \n" | tee -a $list_path
+	echo -ne "$num $line \n" >>  $list_path
+	printf -- "\033[${colour}m %s %s \033[0m\n" "$num" "$line"
 	num=$(($num + 1))
+        if [ $colour -eq 34 ]; then
+		colour=$((colour + 1))
+	elif [ $colour -eq 35 ]; then
+		colour=$((colour - 1))
+	fi
 done
 
 echo -e "\nServer to connect:"
