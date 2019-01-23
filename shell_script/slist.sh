9a37176b7eeffbd8baf62bea0aaef17db708dfce
Alvin Siew <alvin_siew@tech.gov.sg>
2019-01-23 17:12:59 +0800

    make script to be more robust

--- shell_script/slist.sh
+++ shell_script/slist.sh
@@ -2,27 +2,51 @@
 
 list_path=/tmp/serverslist.lst
 
-rm -f $list_path
-
 list=`cat ~/.ssh/config | grep "Host " | awk '{print $2}'`
-num=1
 
-colour=34
-for line in $list
-do
-	sn=$num
-	echo -ne "$num $line \n" >>  $list_path
-	printf -- "\033[${colour}m %s %s \033[0m\n" "$num" "$line"
-	num=$(($num + 1))
-        if [ $colour -eq 34 ]; then
-		colour=$((colour + 1))
-	elif [ $colour -eq 35 ]; then
-		colour=$((colour - 1))
-	fi
-done
+main() {
+	rm -f $list_path
+	num=1
+	colour=34
+	for line in $list
+	do
+		echo -ne "$num $line \n" >>  $list_path
+		printf -- "\033[${colour}m %s %s \033[0m\n" "$num" "$line"
+		num=$(($num + 1))
+        	if [ $colour -eq 34 ]; then
+			colour=$((colour + 1))
+		elif [ $colour -eq 35 ]; then
+			colour=$((colour - 1))
+		fi
+	done
 
-echo -e "\nServer to connect:"
-read cs
+	echo -e "\nServer to connect:"
+	read cs
 
-host=`grep "^$cs " ${list_path} | awk '{print $2}'`
-ssh $host
+        if [[ -z $cs ]]; then
+		clear
+                echo "Enter a number"
+                main
+        fi
+
+        if [[ $cs == *[a-zA-Z]* ]]; then
+		clear
+                echo "Not a number"
+                main
+        fi
+
+        num=$(($num - 1))
+
+        if [ $cs -gt $num ]; then
+		clear
+                echo "Number of out of range"
+                main
+        fi
+
+
+	host=`grep "^$cs " ${list_path} | awk '{print $2}'`
+	ssh $host
+	exit
+}
+
+main
